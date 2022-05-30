d0c47dbdda887f711f4a6e77bf5cbeda631d4f32
Poshqed <98647334+Poshqed@users.noreply.github.com>
2022-05-30 15:29:25 +0100

    functional now

--- scripts.js
+++ scripts.js
@@ -5,13 +5,37 @@
 */
 
 // Query selectors
+nameGenerator = document.querySelector('.js-name');
+emailGenerator = document.querySelector('.js-email');
+addContact = document.getElementById('register-btn')
+displayContact = document.querySelector('#display-btn');
+section = document.querySelector('.js-displayArea')
+
+
 
 
 
 // Event listeners
-
-
+addContact.addEventListener('click', addContactToList);
+displayContact.addEventListener('click', displayContactList);
 
 
 // Functions
+function addContactToList() {
+    localStorage.setItem('newContact', nameGenerator.value);
+    localStorage.setItem('newEmail', emailGenerator.value);
+    alert('Contact added to list');
+}
 
+
+function displayContactList() {
+    var contact = localStorage.getItem('newContact')
+    var email = localStorage.getItem('newEmail')
+    var contactName = document.createElement('p')
+    contactName.innerText = `name: ${contact}`
+    section.appendChild(contactName);
+
+    var contactEmail = document.createElement('p')
+    contactEmail.innerText = `email: ${email}`
+    section.appendChild(contactEmail);
+}
